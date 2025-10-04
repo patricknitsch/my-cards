787812dbbb4b2d24e03dba4a1845f9ede857a6b5
patricknitsch <114565561+patricknitsch@users.noreply.github.com>
2025-10-04 20:47:29 +0200

    Update my-slider.ts

--- src/cards/my-slider.ts
+++ src/cards/my-slider.ts
@@ -409,16 +409,15 @@ export class MySliderV2 extends LitElement {
                 }
                 else if (defaultConfig.mode === 'white') {
 					tmpVal = 66
-					break
-					if (this.entity.state !== 'on') break
-					tmpVal = tmpVal == 0 ? 44 : tmpVal
-					this.oldVal = parseFloat(this.entity.attributes.rgbw_color[3])
-					tmpVal = parseFloat(this.entity.attributes.rgbw_color[3])
-                    if (!defaultConfig.showMin && defaultConfig.min) { // Subtracting savedMin to make slider 0 be far left
-                        tmpVal = tmpVal - defaultConfig.min
-                    }
-                    tmpVal = (tmpVal * (100 - defaultConfig.sliderMin) / 100) + defaultConfig.sliderMin
-                    tmpVal = tmpVal < defaultConfig.sliderMin ? defaultConfig.sliderMin : tmpVal
+					//if (this.entity.state !== 'on') break
+					//tmpVal = tmpVal == 0 ? 44 : tmpVal
+					//this.oldVal = parseFloat(this.entity.attributes.rgbw_color[3])
+					//tmpVal = parseFloat(this.entity.attributes.rgbw_color[3])
+                    //if (!defaultConfig.showMin && defaultConfig.min) { // Subtracting savedMin to make slider 0 be far left
+                    //    tmpVal = tmpVal - defaultConfig.min
+                    //}
+                    //tmpVal = (tmpVal * (100 - defaultConfig.sliderMin) / 100) + defaultConfig.sliderMin
+                    //tmpVal = tmpVal < defaultConfig.sliderMin ? defaultConfig.sliderMin : tmpVal
                 }
                 else if (defaultConfig.mode === 'temperature') {
                     if (this.entity.state !== 'on') break
